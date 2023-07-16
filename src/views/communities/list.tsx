59b06e6151591596349c43308ebe04fa497f0ef5
ethan <graf.ethan@gmail.com>
2023-07-16 13:39:52 -0400

    Create some basic routes

---
+++ src/views/communities/list.tsx
@@ -0,0 +1,5 @@
+export default function CommunityList() {
+    return (
+        <p>CommunityList</p>
+    )
+}
---
+++ src/views/dex/dex.tsx
@@ -0,0 +1,5 @@
+export default function Dex() {
+    return (
+        <p>Dex</p>
+    )
+}
